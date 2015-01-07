6812310766a34017730cf5e03650aff8012efe0a
Brian Tenggren <briantenggren@Brians-MacBook-Pro.local>
2015-01-07 11:48:03 -0500

    Change the spec_helper file to match book

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,21 @@
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
+
+require 'rspec/rails'
 require 'webmock/rspec'
 
-# http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
+
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
   end
 
-  config.mock_with :rspec do |mocks|
-    mocks.syntax = :expect
-  end
-
-  config.order = :random
+  config.fail_fast = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = 'random'
+  config.use_transactional_fixtures = false
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
