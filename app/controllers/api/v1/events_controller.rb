474a6aa24c1571b6cc761521d5fbfbfa6bbd087e
Brian Tenggren <briantenggren@Brians-MacBook-Pro.local>
2015-01-07 17:40:45 -0500

    add a create method to post with route and view

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,4 +2,34 @@ class Api::V1::EventsController < ApiController
   def show
     @event = Event.find(params[:id])
   end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      render
+    end
+  end
+
+  private
+
+  def event_params
+    {
+      address: params[:address],
+      ended_at: params[:ended_at],
+      lat: params[:lat],
+      lon: params[:lon],
+      name: params[:name],
+      started_at: params[:started_at],
+      owner: user
+    }
+  end
+
+  def user
+    User.find_or_create_by(device_token: device_token)
+  end
+
+  def device_token
+    params[:owner].try(:[], :device_token)
+  end
 end
--- spec/requests/api/v1/events/events_spec.rb
+++ spec/requests/api/v1/events/events_spec.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 describe 'GET /v1/events/:id' do
   it 'returns an event by :id' do
     event = create(:event)
@@ -18,5 +20,37 @@ describe 'GET /v1/events/:id' do
         }
       }
     )
+  end
+end
+
+describe 'POST /v1/events' do
+  it 'saves the address, lat, lon, name, and started_at date' do
+    date = Time.current
+    lat = Faker::Address.latitude.to_f.round(5)
+    lon = Faker::Address.longitude.to_f.round(5)
+    device_token = '123abc456xyz'
+    owner = create(:user, device_token: device_token)
+
+    post '/v1/events', {
+      address: '123 Main St.',
+      ended_at: date,
+      lat: lat,
+      lon: lon,
+      name: 'Fun Happy Time!',
+      started_at: date,
+      owner: {
+        device_token: device_token
+      }
+    }.to_json, { 'Content-Type' => 'application/json' }
+
+    event = Event.last
+    expect(response_json).to eq({ 'id' => event.id })
+    expect(response_json).to eq '123 Main St.'
+    expect(event.ended_at.to_i).to eq date.to_i
+    expect(event.lat).to eq lat
+    expect(event.lon).to eq lon
+    expect(event.name).to eq 'Fun Happy Time!'
+    expect(event.started_at.to_i).to eq date.to_i
+    expect(event.owner).to eq owner
   end
 end
