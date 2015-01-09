b2a4d5dfbc145afc290e118ec0aa05596eb65e40
Brian Tenggren <briantenggren@Brians-MacBook-Pro.local>
2015-01-09 17:01:08 -0500

    remove uneccesary patch request attrs

--- spec/requests/api/v1/events/events_spec.rb
+++ spec/requests/api/v1/events/events_spec.rb
@@ -89,12 +89,7 @@ describe 'Events Requests' do
       event = create(:event, name: 'Old Name')
       new_name = 'New Name'
       event_params = {
-        address: event.address,
-        ended_at: event.ended_at,
-        lat: event.lat,
-        lon: event.lon,
         name: new_name,
-        started_at: event.started_at,
         owner: {
           device_token: event.owner.device_token
         }
