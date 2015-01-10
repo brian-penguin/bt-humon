26427660936f9fd3472576055affdc48c1f1f422
Brian Tenggren <briantenggren@Brians-MacBook-Pro.local>
2015-01-10 13:45:36 -0500

    Add events namespace for finding events by distance

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   scope module: :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :events, only: [:show, :create, :update]
+      namespace :events do
+        resources :nearests, only: [:indesx]
+      end
     end
   end
 end
