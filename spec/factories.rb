1b8a5704af12148510f212f02801160d4547969d
Brian Tenggren <briantenggren@Brians-MacBook-Pro.local>
2015-01-12 15:11:11 -0500

    Add traits to events factory to better understand geocoding tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -18,6 +18,26 @@ FactoryGirl.define do
     name
     started_at
     owner factory: :user
+
+    trait :boston do
+      lat 42.36701
+      lon (-71.08021)
+    end
+
+    trait :worcester do
+      lat 42.25975
+      lon (-71.78677)
+    end
+
+    trait :springfield do
+      lat 42.09955
+      lon (-72.58538)
+    end
+
+    trait :san_fran do
+      lat 37.77188
+      lon (-122.40522)
+    end
   end
 
   factory :user do
--- spec/requests/api/v1/events/nearest_spec.rb
+++ spec/requests/api/v1/events/nearest_spec.rb
@@ -1,18 +1,14 @@
 describe 'Events Requests' do
   describe 'GET /v1/events/nearests?lat=&lon=&radius=' do
     it 'returns events closest to lat and lon' do
-      near_event = create(:event,
-                          lat: 37.760322,
-                          lon: -122.429667)
-      farther_event = create(:event,
-                             lat: 37.760321,
-                             lon: -122.429667)
-      create(:event, lat: 37.687737, lon: -122.470608)
-      request_lat = 37.771098
-      request_lon = -122.430782
-      request_radius = 5
+      near_event = create(:event, :worcester)
+      farther_event = create(:event, :springfield)
+      boston_event = attributes_for(:event, :boston)
+      create(:event, :san_fran)
+      # distance is measured in kilometers (Mass is about 306 km across)
+      radius = 306
 
-      get "/v1/events/nearests?lat=#{request_lat}&lon=#{request_lon}&radius=#{request_radius}"
+      get "/v1/events/nearests?lat=#{boston_event[:lat]}&lon=#{boston_event[:lon]}&radius=#{radius}"
 
       expect(response).to have_http_status(:ok)
       expect(response_json).to eq([
