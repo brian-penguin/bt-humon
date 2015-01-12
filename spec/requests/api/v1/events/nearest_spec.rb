7c36416c01b0952f9d2ee543ef3662ae23a8f932
Brian Tenggren <briantenggren@Brians-MacBook-Pro.local>
2015-01-12 17:12:20 -0500

    Add nearests array order expectation

--- spec/requests/api/v1/events/nearest_spec.rb
+++ spec/requests/api/v1/events/nearest_spec.rb
@@ -14,40 +14,8 @@ describe 'Events Requests' do
 
       expect(response).to have_http_status(:ok)
       expect(response).to match_response_schema(:nearests)
-      # expect(response_json).to eq([
-      #   {
-      #     'address' => near_event.address,
-      #     'created_at' => near_event.created_at.as_json,
-      #     'ended_at' => near_event.ended_at,
-      #     'id' => near_event.id,
-      #     'lat' => near_event.lat,
-      #     'lon' => near_event.lon,
-      #     'name' => near_event.name,
-      #     'started_at' => near_event.started_at.as_json,
-      #     'updated_at' => near_event.updated_at.as_json,
-      #     'owner' => {
-      #       'created_at' => near_event.owner.created_at.as_json,
-      #       'device_token' => near_event.owner.device_token,
-      #       'updated_at' => near_event.owner.updated_at.as_json
-      #     }
-      #   },
-      #   {
-      #     'address' => farther_event.address,
-      #     'created_at' => farther_event.created_at.as_json,
-      #     'ended_at' => farther_event.ended_at,
-      #     'id' => farther_event.id,
-      #     'lat' => farther_event.lat,
-      #     'lon' => farther_event.lon,
-      #     'name' => farther_event.name,
-      #     'started_at' => farther_event.started_at.as_json,
-      #     'updated_at' => farther_event.updated_at.as_json,
-      #     'owner' => {
-      #       'created_at' => farther_event.owner.created_at.as_json,
-      #       'device_token' => farther_event.owner.device_token,
-      #       'updated_at' => farther_event.owner.updated_at.as_json
-      #     }
-      #   }
-      # ])
+      expect(response_json.first['id']).to eq(near_event.id)
+      expect(response_json.last['id']).to eq(farther_event.id)
     end
     it 'returns an error message if no event is found' do
       request_lat = 37.771098
